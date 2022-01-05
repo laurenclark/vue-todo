261a468e5b593a30985fd9eac2d255092bdc72cd
Lauren Clark <lauren.c4231@gmail.com>
2022-01-05 20:49:40 +0000

    Add the start of basic unit tests

--- src/components/TodoForm.spec.js
+++ src/components/TodoForm.spec.js
@@ -5,3 +5,18 @@ describe("is the TodoForm Component", () => {
         expect(TodoForm).toBeAComponent();
     });
 });
+
+describe("Blank slate of the form", () => {
+    it("Has a text input", () => {});
+    it("Has placeholder text in the text input", () => {
+        const expected = "Wash dishes";
+        const actual = "";
+        expect(actual).toBe(expected);
+    });
+    it("Has a date picker input", () => {});
+    it("Has a button labelled 'submit'", () => {});
+});
+
+describe("Creates a Todo Object", () => {
+    expect(true).toBe(false);
+});
