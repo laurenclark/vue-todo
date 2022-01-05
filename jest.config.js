b8c0669188d2bf884aa410d2f9e52dcc87ba84e2
Lauren Clark <lauren.c4231@gmail.com>
2022-01-05 19:48:33 +0000

    Add custom matchers and jest conf

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,25 @@
 module.exports = {
     verbose: true,
     roots: ["<rootDir>/src/", "<rootDir>/specs/"],
-    moduleFileExtensions: ["js", "vue"],
+    moduleFileExtensions: ["js", "json", "vue"],
     moduleNameMapper: {
         "^@/(.*)$": "<rootDir>/src/components/$1"
     },
     transform: {
+        "^.+\\.vue$": "vue-jest",
         "^.+\\.js$": "babel-jest",
-        "^.+\\.vue$": "vue-jest"
+        ".+\\.(css|scss|jpe?g|png|gif|webp|svg|mp4|webm|ogg|mp3|wav|flac|aac|woff2?|eot|ttf|otf)$":
+            "jest-transform-stub"
     },
-    snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"]
+    setupFilesAfterEnv: ["<rootDir>/matchers"],
+    snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
+    globals: {
+        "vue-jest": {
+            // Compilation errors in the <style> tags of Vue components will
+            // already result in failing builds, so compiling CSS during unit
+            // tests doesn't protect us from anything. It only complicates
+            // and slows down our unit tests.
+            experimentalCSSCompile: false
+        }
+    }
 };
---
+++ matchers.js
@@ -0,0 +1,31 @@
+// See these docs for details on Jest's matcher utils:
+// https://facebook.github.io/jest/docs/en/expect.html#thisutils
+
+const _ = require("lodash");
+const customMatchers = {};
+
+customMatchers.toBeAComponent = function (options) {
+    if (isAComponent()) {
+        return {
+            message: () =>
+                `expected ${this.utils.printReceived(
+                    options
+                )} not to be a Vue component`,
+            pass: true
+        };
+    } else {
+        return {
+            message: () =>
+                `expected ${this.utils.printReceived(
+                    options
+                )} to be a valid Vue component, exported from a .vue file`,
+            pass: false
+        };
+    }
+
+    function isAComponent() {
+        return _.isPlainObject(options) && typeof options.render === "function";
+    }
+};
+
+global.expect.extend(customMatchers);
